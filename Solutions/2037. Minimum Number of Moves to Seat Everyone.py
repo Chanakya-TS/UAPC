fdd6ef9b1734a5c17efc4f2e81848453574e17d4
Chanakya Thirumala Setty <66557279+Chanakya-TS@users.noreply.github.com>
2024-09-05 12:31:51 -0500

    Create 2037. Minimum Number of Moves to Seat Everyone.py

---
+++ Solutions/2037. Minimum Number of Moves to Seat Everyone.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minMovesToSeat(self, seats, students):
+        """
+        :type seats: List[int]
+        :type students: List[int]
+        :rtype: int
+        """
+        answer = 0
+        n = len(seats)
+        
+        # Sort both lists
+        seats.sort()
+        students.sort()
+
+        for i in range(n):
+            # student[i] needs to go to ith seat
+            # abs(student[i] - seats[i]) is the distance the student needs to travel
+            answer += abs(students[i] - seats[i])
+
+        return answer
